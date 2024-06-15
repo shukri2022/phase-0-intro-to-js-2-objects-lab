29783455a1b6aa98804e25abbdf650b11c43a25e
Shukri Abdi <shukri751@gmail.com>
2024-06-16 00:57:38 +0300

    function created on index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+  return {
+      ...employee,
+      [key]: value  
+  };
+}
+
+
+let employee = {
+  name: "John Doe",
+  streetAddress: "123 Main St"
+};
+
+let updatedEmployee = updateEmployeeWithKeyAndValue(employee, "name", "Jane Smith");
+console.log(updatedEmployee);
+
+
+console.log(employee);
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value; 
+  return employee; 
+}
+function deleteFromEmployeeByKey(employee, key) {
+  let newEmployee = { ...employee }; 
+  delete newEmployee[key]; 
+    return newEmployee; 
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key]; 
+  return employee; 
+}
